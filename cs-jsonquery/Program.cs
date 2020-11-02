5601ade4e515e8d3aa4e088968e0f2ede9240f19
cnra <roser137@hotmail.com>
2020-11-02 11:48:55 +0300

    init

---
+++ cs-jsonquery/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+namespace cs_jsonquery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var txt = File.ReadAllText("data.json");
+            JObject js = JObject.Parse(txt);
+            Console.WriteLine(js["header"]["id"]);
+        }
+    }
+}
